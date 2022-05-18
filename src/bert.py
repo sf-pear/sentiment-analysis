2a837a3c0040727739087b6b32ef6c738cb7d043
Sabrina Pereira <sabrinafp35@gmail.com>
2022-05-18 23:31:44 +0200

    Create bert.py

---
+++ src/bert.py
@@ -0,0 +1,279 @@
+import csv
+import re
+import numpy as np
+import pandas as pd
+import torch
+from transformers import BertTokenizer
+from datetime import datetime
+import pickle
+from torch.utils.data import TensorDataset, DataLoader, RandomSampler, SequentialSampler
+import random
+import time
+import torch.nn as nn
+from transformers import DistilBertTokenizer, DistilBertModel
+
+
+
+# Create a function to tokenize a set of texts
+def preprocessing_for_bert(data, MAX_LEN=512):
+    """Perform required preprocessing steps for pretrained BERT.
+    @param    data (np.array): Array of texts to be processed.
+    @return   input_ids (torch.Tensor): Tensor of token ids to be fed to a model.
+    @return   attention_masks (torch.Tensor): Tensor of indices specifying which
+                  tokens should be attended to by the model.
+    """
+    # Create empty lists to store outputs
+    input_ids = []
+    attention_masks = []
+    tokenizer = DistilBertTokenizer.from_pretrained('distilbert-base-uncased', do_lower_case=True)
+    # For every sentence...
+    for sent in data:
+        # `encode_plus` will:
+        #    (1) Tokenize the sentence
+        #    (2) Add the `[CLS]` and `[SEP]` token to the start and end
+        #    (3) Truncate/Pad sentence to max length
+        #    (4) Map tokens to their IDs
+        #    (5) Create attention mask
+        #    (6) Return a dictionary of outputs
+        encoded_sent = tokenizer.encode_plus(
+            text=sent,  # Preprocess sentence
+            add_special_tokens=True,        # Add `[CLS]` and `[SEP]`
+            max_length=MAX_LEN,             # Max length to truncate/pad
+            padding='max_length',           # Pad sentence to max length
+            #return_tensors='pt',           # Return PyTorch tensor
+            return_attention_mask=True,     # Return attention mask
+            truncation = True
+            )
+        
+        # Add the outputs to the lists
+        input_ids.append(encoded_sent.get('input_ids'))
+        attention_masks.append(encoded_sent.get('attention_mask'))
+
+    # Convert lists to tensors
+    input_ids = torch.tensor(input_ids)
+    attention_masks = torch.tensor(attention_masks)
+
+    return input_ids, attention_masks
+
+# Trainer
+# Create the BertClassifier class
+class BertClassifier(nn.Module):
+    """Bert Model for Classification Tasks.
+    """
+    def __init__(self, freeze_bert=False):
+        """
+        @param    bert: a DistilBertModel object
+        @param    classifier: a torch.nn.Module classifier
+        @param    freeze_bert (bool): Set `False` to fine-tune the BERT model
+        """
+        super(BertClassifier, self).__init__()
+        # Specify hidden size of BERT, hidden size of our classifier, and number of labels
+        D_in, H, D_out = 768, 50, 2
+
+        # Instantiate BERT model
+        self.bert = DistilBertModel.from_pretrained('distilbert-base-uncased')
+
+        # Instantiate an one-layer feed-forward classifier
+        self.classifier = nn.Sequential(
+            nn.Linear(D_in, H),
+            nn.ReLU(),
+            #nn.Dropout(0.5),
+            nn.Linear(H, D_out)
+        )
+
+        # Freeze the BERT model
+        if freeze_bert:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+        
+    def forward(self, input_ids, attention_mask):
+        """
+        Feed input to BERT and the classifier to compute logits.
+        @param    input_ids (torch.Tensor): an input tensor with shape (batch_size, max_length)
+        @param    attention_mask (torch.Tensor): a tensor that hold attention mask
+                      information with shape (batch_size, max_length)
+        @return   logits (torch.Tensor): an output tensor with shape (batch_size, num_labels)
+        """
+        # Feed input to BERT
+        outputs = self.bert(input_ids=input_ids,
+                            attention_mask=attention_mask)
+        
+        # Extract the last hidden state of the token `[CLS]` for classification task
+        last_hidden_state_cls = outputs[0][:, 0, :]
+
+        # Feed input to classifier to compute logits
+        logits = self.classifier(last_hidden_state_cls)
+
+        return logits
+
+# optimiser
+from transformers import AdamW, get_linear_schedule_with_warmup
+
+def initialize_model2(step, epochs=4):
+    """Initialize the Bert Classifier, the optimizer and the learning rate scheduler.
+    """
+    # Instantiate Bert Classifier
+    bert_classifier = BertClassifier(freeze_bert=False)
+
+    # Tell PyTorch to run the model on GPU
+    if torch.cuda.is_available():       
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    bert_classifier.to(device)
+
+    # Create the optimizer
+    optimizer = AdamW(bert_classifier.parameters(),
+                      lr=5e-5,    # Default learning rate
+                      eps=1e-8    # Default epsilon value
+                      )
+
+    # Total number of training steps
+    total_steps = step * epochs
+
+    # Set up the learning rate scheduler
+    scheduler = get_linear_schedule_with_warmup(optimizer,
+                                                num_warmup_steps=0, # Default value
+                                                num_training_steps=total_steps)
+    return bert_classifier, optimizer, scheduler
+
+def set_seed(seed_value=42):
+    """Set seed for reproducibility.
+    """
+    random.seed(seed_value)
+    np.random.seed(seed_value)
+    torch.manual_seed(seed_value)
+    torch.cuda.manual_seed_all(seed_value)
+
+from tqdm import tqdm
+
+def train(model, optimizer, scheduler, train_dataloader, val_dataloader=None, epochs=4, evaluation=False):
+    """
+    Train the BertClassifier model.
+    """
+    loss_fn = nn.CrossEntropyLoss()
+
+    if torch.cuda.is_available():       
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+        
+    # Start training loop
+    for epoch_i in range(epochs):
+        # =======================================
+        #               Training
+        # =======================================
+
+        # Measure the elapsed time of each epoch
+        t0_epoch, t0_batch = time.time(), time.time()
+
+        # Reset tracking variables at the beginning of each epoch
+        total_loss, batch_loss, batch_counts = 0, 0, 0
+
+        # Put the model into the training mode
+        model.train()
+        
+
+        # For each batch of training data...
+        for step, batch in enumerate(tqdm(train_dataloader)):
+            batch_counts +=1
+            # Load batch to GPU
+            b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
+
+            # Zero out any previously calculated gradients
+            model.zero_grad()
+
+            # Perform a forward pass. This will return logits.
+            logits = model(b_input_ids, b_attn_mask)
+
+            # Compute loss and accumulate the loss values
+            loss = loss_fn(logits, b_labels)
+            batch_loss += loss.item()
+            total_loss += loss.item()
+
+            # Perform a backward pass to calculate gradients
+            loss.backward()
+
+            # Clip the norm of the gradients to 1.0 to prevent "exploding gradients"
+            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+
+            # Update parameters and the learning rate
+            optimizer.step()
+            scheduler.step()
+
+            # Print the loss values and time elapsed for every 20 batches
+            if (step % 20 == 0 and step != 0) or (step == len(train_dataloader) - 1):
+                # Calculate time elapsed for 20 batches
+                time_elapsed = time.time() - t0_batch
+
+                # Print training results
+                #print(f"{epoch_i + 1:^7} | {step:^7} | {batch_loss / batch_counts:^12.6f} | {'-':^10} | {'-':^9} | {time_elapsed:^9.2f}")
+
+                # Reset batch tracking variables
+                batch_loss, batch_counts = 0, 0
+                t0_batch = time.time()
+
+        # Calculate the average loss over the entire training data
+        avg_train_loss = total_loss / len(train_dataloader)
+
+        # =======================================
+        #               Evaluation
+        # =======================================
+        if evaluation == True:
+            # After the completion of each training epoch, measure the model's performance
+            # on our validation set.
+            val_loss, val_accuracy = evaluate(model, val_dataloader)
+
+            # Print performance over the entire training data
+            time_elapsed = time.time() - t0_epoch
+                
+    # return total_loss # add this so we can plot the loss
+
+def evaluate(model, val_dataloader):
+    """
+    After the completion of each training epoch, measure the model's performance
+    on our validation set.
+    """
+
+    # Tell PyTorch to run the model on GPU
+    if torch.cuda.is_available():       
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    loss_fn = nn.CrossEntropyLoss()
+
+    # Put the model into the evaluation mode. The dropout layers are disabled during
+    # the test time.
+    model.eval()
+
+    # Tracking variables
+    val_accuracy = []
+    val_loss = []
+
+    # For each batch in our validation set...
+    for batch in val_dataloader:
+        # Load batch to GPU
+        b_input_ids, b_attn_mask, b_labels = tuple(t.to(device) for t in batch)
+
+        # Compute logits
+        with torch.no_grad():
+            logits = model(b_input_ids, b_attn_mask)
+
+        # Compute loss
+        loss = loss_fn(logits, b_labels)
+        val_loss.append(loss.item())
+
+        # Get the predictions
+        preds = torch.argmax(logits, dim=1).flatten()
+
+        # Calculate the accuracy rate
+        accuracy = (preds == b_labels).cpu().numpy().mean() * 100
+        val_accuracy.append(accuracy)
+
+    # Compute the average accuracy and loss over the validation set.
+    val_loss = np.mean(val_loss)
+    val_accuracy = np.mean(val_accuracy)
+
+    return val_loss, val_accuracy
